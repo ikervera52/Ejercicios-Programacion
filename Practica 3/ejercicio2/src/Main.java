7cdf7d6e859d8a5fa7a04e727f14e58751370f87
Iker <iker.vera.52898@ikasle.egibide.org>
2025-12-11 12:27:14 +0100

    Practica3/ejercicio2 añadido

---
+++ Practica 3/ejercicio2/src/Main.java
@@ -0,0 +1,109 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Main {
+
+    public static Scanner sc;
+
+    public static LinkedList <String> productos;
+    public static LinkedList <LocalDate> fechas;
+    public static void main(String[] args) {
+
+        inicializarListas();
+        pedirProductos();
+        borrarCaducados();
+
+    }
+    public static void inicializarListas(){
+        sc = new Scanner(System.in);
+
+        productos = new LinkedList<>();
+        fechas = new LinkedList<>();
+    }
+
+    public static void pedirProductos(){
+        System.out.println("-- Guardado de productos --");
+        boolean continuar = true;
+        do {
+            try{
+                Pattern patron = Pattern.compile("^[a-z]+$");
+                System.out.print("Nombre del producto: ");
+                String nombre = sc.nextLine();
+                Matcher m = patron.matcher(nombre);
+                if(!m.matches()){
+                    throw new Error();
+                }
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                System.out.print("Fecha de caducidad: ");
+                LocalDate fecha = LocalDate.parse(sc.nextLine(), dtf);
+
+                productos.add(nombre);
+                fechas.add(fecha);
+
+                continuar = preguntarContinuar();
+
+            }
+            catch(Error e){
+                System.out.println("** Error en el procedimiento **");
+            }
+            catch(DateTimeParseException ex){
+                System.out.println("** Fecha no valida **");
+            }
+        }while(continuar);
+    }
+    public static boolean preguntarContinuar(){
+        boolean error = true;
+        boolean continuar = true;
+        do {
+            try{
+                System.out.print("Quieres añadir mas productos (si/no): ");
+                String respuesta = sc.nextLine();
+                if(respuesta.equals("no")){
+                    System.out.println("\n-- Productos guardados --");
+                    imprimirLista();
+                    continuar = false;
+                    error = false;
+                } else if(respuesta.equals("si")){
+                    error = false;
+                }
+                else throw new Error();
+            }
+            catch (Error e){
+                System.out.println("** Opción no valida **");
+            }
+        } while(error);
+        return continuar;
+
+    }
+
+    public static void borrarCaducados(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.print("Desde que fecha quieres descartar productos: ");
+        LocalDate fecha = LocalDate.parse(sc.nextLine(), dtf);
+
+        LinkedList <LocalDate> caducados = new LinkedList<>(fechas);
+
+        for (LocalDate fechasAnteriores: caducados){
+            if (fechasAnteriores.isBefore(fecha)){
+                fechas.remove();
+                productos.remove();
+            }
+        }
+
+        System.out.println("\n-- Productos restantes --");
+        imprimirLista();
+    }
+
+    public static void imprimirLista(){
+        int repetir = productos.size();
+        for(int i = 0; i < repetir; i++){
+            System.out.println("Nombre: " + productos.get(i) + " Fecha: " + fechas.get(i));
+        }
+    }
+}
